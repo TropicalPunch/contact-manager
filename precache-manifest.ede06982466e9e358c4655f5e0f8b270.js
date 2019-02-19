76b455bc9f2dbfe7643c84aff544dfc4559e972e
TropicalPunch <billabang1@hotmail.com>
2019-02-19 15:23:05 +0200

    Updates

---
+++ precache-manifest.ede06982466e9e358c4655f5e0f8b270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "a5e86dd71e7a8c54e5b3",
+    "url": "/contact-manager/static/js/main.a5e86dd7.chunk.js"
+  },
+  {
+    "revision": "23136252e1ff2eb1a192",
+    "url": "/contact-manager/static/js/1.23136252.chunk.js"
+  },
+  {
+    "revision": "a5e86dd71e7a8c54e5b3",
+    "url": "/contact-manager/static/css/main.266afd20.chunk.css"
+  },
+  {
+    "revision": "23136252e1ff2eb1a192",
+    "url": "/contact-manager/static/css/1.dd6ba800.chunk.css"
+  },
+  {
+    "revision": "4f060c8cfe2087d1a7115cc7e334b66b",
+    "url": "/contact-manager/index.html"
+  }
+];
